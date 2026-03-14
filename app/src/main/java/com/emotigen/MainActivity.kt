796cc7fa080f61d4a526f5bde1d8ba70c31b85a2
ix2blood-oss <ix2blood@gmail.com>
2026-03-15 03:57:07 +0900

    Update MainActivity.kt

--- app/src/main/java/com/emotigen/MainActivity.kt
+++ app/src/main/java/com/emotigen/MainActivity.kt
@@ -1,1 +1,33 @@
+package com.emotigen
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity:AppCompatActivity(){
+
+override fun onCreate(savedInstanceState:Bundle?){
+super.onCreate(savedInstanceState)
+setContentView(R.layout.activity_main)
+
+val btn=findViewById<Button>(R.id.generate)
+val result=findViewById<TextView>(R.id.result)
+
+btn.setOnClickListener{
+
+val emojis=EmojiEngine.generate()
+
+val builder=StringBuilder()
+
+emojis.forEachIndexed{index,e->
+builder.append("${index+1}. ${e.action} - ${e.line}\n")
+}
+
+result.text=builder.toString()
+
+}
+
+}
+
+}
